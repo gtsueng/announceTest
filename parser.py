a593701659ed3fad51859c47c9e9540bb58608f5
ginger <gtsueng@gmail.com>
2023-04-27 16:10:37 -0700

    update request path

--- parser.py
+++ parser.py
@@ -1,20 +1,21 @@
 import json
+import os
 import requests
 
 from biothings import config
 logger = config.logger
 
 def load_filenames():
-    r = requests.get('https://github.com/gtsueng/covid_announcements/blob/main/San%20Diego%20County/data/processed_list.txt')
+    r = requests.get('https://raw.githubusercontent.com/gtsueng/covid_announcements/main/San%20Diego%20County/data/processed_list.txt')
     reportlist=r.text.split('\n')
     formattedlist = [x.replace(" ","%20") for x in reportlist]
     return(formattedlist)
 
 def load_annotations():
-    basejsonurl = 'https://github.com/gtsueng/covid_announcements/tree/main/San%20Diego%20County/data/processed/'
+    basejsonurl = 'https://raw.githubusercontent.com/gtsueng/covid_announcements/main/San%20Diego%20County/data/processed/'
     formattedlist = load_filenames()
     for eachjson in formattedlist:
-        fileurl = basejsonurl+eachjson
+        fileurl = os.path.join(basejsonurl,eachjson)
         rawdoc = requests.get(fileurl)
         doc = json.loads(rawdoc.text)
         doc['_id'] = eachjson.replace('.json','')
